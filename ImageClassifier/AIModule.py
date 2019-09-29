c46a6dd3b6fab7ce968392f58eb4ad34d752218e
23michael45 <michael2345@live.cn>
2019-09-29 11:55:35 +0800

    add AIModule.py

---
+++ ImageClassifier/AIModule.py
@@ -0,0 +1,12 @@
+
+def getShape(image):
+    #image.show()
+
+    dict = { 0:'pentagon',
+    1:'octagon',
+    2:'hexagon',
+    3:'circle',
+    4:'rectangular',
+    5:'square'}
+
+    return dict[4]
