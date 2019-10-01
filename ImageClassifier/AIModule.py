4dcbd0de0fa48eb361f00248bd0810be8d7f8907
michael <michael2345@live.cn>
2019-10-01 10:23:48 +0800

    add shape detection opencv

--- ImageClassifier/AIModule.py
+++ ImageClassifier/AIModule.py
@@ -1,12 +1,73 @@
+import cv2
+import numpy as np
+import sys
+shapeDict = {-1:'none', 
+        0:'pentagon',
+        1:'octagon',
+        2:'hexagon',
+        3:'circle',        
+        4:'square',
+        5:'rectangular'}
+def approx2shape(approx):
+    vertexCount = approx.shape[0]
+    
+    if vertexCount == 5:
+        return 0
+    elif vertexCount == 8:
+        return 1
+    elif vertexCount == 6:
+        return 2
+    elif vertexCount == 4:
+        e1 = np.linalg.norm(approx[0] - approx[1])
+        e2 = np.linalg.norm(approx[1] - approx[2])
+        rate = 1.2
+        if e1 > e2 * rate or e1 < e2 / rate:
+            return 5
+        else:
+            return 4
+
+        return 4
+    elif vertexCount >= 10:
+        return 3
+    return -1;
+
+def findLargestContour(contours):
+    maxPerimeter = sys.float_info.min
+    maxIndex = -1
+    for i in np.arange(len(contours)):
+        contour = contours[i]
+        
+        area = cv2.contourArea(contour)
+        perimeter = cv2.arcLength(contour, True)
+        M = cv2.moments(contour)
+        cx, cy = M['m10'] / M['m00'], M['m01'] / M['m00'] 
+
+        if(perimeter > maxPerimeter):
+            maxIndex = i
+    return maxIndex,contours[maxIndex]
 
 def getShape(image):
     #image.show()
 
-    dict = { 0:'pentagon',
-    1:'octagon',
-    2:'hexagon',
-    3:'circle',
-    4:'rectangular',
-    5:'square'}
 
-    return dict[4]
+
+    img2gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+    _, thresh = cv2.threshold(img2gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    image, contours, hierarchy = cv2.findContours(thresh, 3, 2)
+
+    miaxIndex,maxContour = findLargestContour(contours)
+
+    epsilon = 0.01*cv2.arcLength(maxContour,True)
+    approx = cv2.approxPolyDP(maxContour,epsilon,True)
+
+    return shapeDict[approx2shape(approx)]
+
+
+def test():
+    image = cv2.imread("D:/DevelopProj/Dadao/ESP32Project/Datasets/Shapes/Dst/tfrecord/shapes_photos/5/5_15.jpg")
+    ret = getShape(image)
+    print(ret)
+
+if __name__ == '__main__':
+    test()
